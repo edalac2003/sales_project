e01f81cb340bf8241305df5c3338c464a7609dcd
Edwin Acosta Bravo <edalac2003@yahoo.com.ar>
2023-04-26 14:38:02 -0500

    Create State module

--- Sales Project/Sales.Shared/Entities/State.cs
+++ Sales Project/Sales.Shared/Entities/State.cs
@@ -7,7 +7,7 @@ namespace Sales.Shared.Entities
     {
         public int Id { get; set; }
 
-        [Display]
+        [Display(Name = "Estado/Departamento")]
         [MaxLength(100, ErrorMessage = "El campo {0} debe tener un máximo de {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Name { get; set; } = null!;    
